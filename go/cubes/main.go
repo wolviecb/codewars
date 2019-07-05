72a697087d5a7e534405fe87124931a08e88b44a
Tom Andrade <wolvie@gmail.com>
2019-07-05 06:07:24 +0200

    shorter/simpler cube

--- go/cubes/main.go
+++ go/cubes/main.go
@@ -1,15 +1,10 @@
 package cube
 
-import (
-	"math"
-)
-
 // FindNb find cubes
 func FindNb(m int) int {
 	for n := 0; ; n++ {
 		if m > 0 {
-			c := math.Pow(float64(n+1), float64(3))
-			m = m - int(c)
+			m -= (n + 1) * (n + 1) * (n + 1)
 		} else if m == 0 {
 			return n
 		} else if m < 0 {
